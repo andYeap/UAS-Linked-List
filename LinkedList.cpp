c83dd36d3ea5692863c3b07c197abc2cf4f52a33
SulthonShobrie <msshobrie@gmail.com>
2025-06-23 23:27:03 +0800

    Membuat fungsi utama

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -316,3 +316,187 @@ void showMenu() {
     cout << "7. Keluar\n";
     cout << "Pilih menu (1-7): ";
 }
+
+void managePlayersMenu(TeamNode* head, string teamName) {
+    TeamNode* team = head;
+    while (team != nullptr && team->teamName != teamName) {
+        team = team->next;
+    }
+
+    if (team == nullptr) {
+        cout << "Tim '" << teamName << "' tidak ditemukan!\n";
+        return;
+    }
+
+    int choice;
+    do {
+        cout << "\n=== KELOLA PLAYER TIM " << teamName << " ===\n";
+        cout << "Player saat ini: " << team->playerCount << "/5\n";
+
+        if (team->playerCount > 0) {
+            cout << "\nDaftar Player:\n";
+            for (int i = 0; i < team->playerCount; i++) {
+                cout << (i + 1) << ". " << team->players[i].nickname 
+                     << " (ID: " << team->players[i].id 
+                     << ", Server: " << team->players[i].server << ")\n";
+            }
+        }
+
+        cout << "\n1. Tambah Player Satuan\n";
+        cout << "2. Tambah 5 Player Sekaligus\n";
+        cout << "3. Hapus Player\n";
+        cout << "4. Ganti Player\n";
+        cout << "5. Kembali ke Menu Utama\n";
+        cout << "Pilih (1-5): ";
+        cin >> choice;
+        cin.ignore();
+
+        string nickname, id, server;
+        int playerNum;
+
+        switch (choice) {
+            case 1:
+                if (team->playerCount >= 5) {
+                    cout << "Tim sudah penuh!\n";
+                } else {
+                    cout << "Nickname Player " << (team->playerCount + 1) << " : ";
+                    getline(cin, nickname);
+                    cout << "ID & Server : ";
+                    getline(cin, id);
+                    cout << "Server : ";
+                    getline(cin, server);
+                    addPlayerToTeam(head, teamName, nickname, id, server);
+                }
+                break;
+
+            case 2:
+                addAllPlayersToTeam(head, teamName);
+                break;
+
+            case 3:
+                if (team->playerCount == 0) {
+                    cout << "Tidak ada player untuk dihapus!\n";
+                } else {
+                    cout << "Masukkan nomor player yang akan dihapus (1-" << team->playerCount << "): ";
+                    cin >> playerNum;
+                    removePlayerFromTeam(head, teamName, playerNum);
+                }
+                break;
+
+            case 4:
+                if (team->playerCount == 0) {
+                    cout << "Tidak ada player untuk diganti!\n";
+                } else {
+                    cout << "Masukkan nomor player yang akan diganti (1-" << team->playerCount << "): ";
+                    cin >> playerNum;
+                    cin.ignore();
+                    cout << "Nickname Player Baru : ";
+                    getline(cin, nickname);
+                    cout << "ID & Server Baru : ";
+                    getline(cin, id);
+                    cout << "Server Baru : ";
+                    getline(cin, server);
+                    replacePlayerInTeam(head, teamName, playerNum, nickname, id, server);
+                }
+                break;
+
+            case 5:
+                cout << "Kembali ke menu utama...\n";
+                break;
+
+            default:
+                cout << "Pilihan tidak valid!\n";
+        }
+
+        if (choice != 5) {
+            cout << "\nTekan Enter untuk melanjutkan...";
+            cin.get();
+        }
+
+    } while (choice != 5);
+}
+
+int main() {
+    TeamNode* head = nullptr;
+    int choice;
+    string teamName;
+
+    cout << "Selamat datang di Sistem Pendaftaran Turnamen Mobile Legends!\n";
+
+    do {
+        showMenu();
+        cin >> choice;
+        cin.ignore();
+
+        switch (choice) {
+            case 1:
+                cout << "Masukkan nama tim: ";
+                getline(cin, teamName);
+                addTeam(head, teamName);
+                break;
+
+            case 2:
+                displayRegisteredTeams(head);
+
+                if (head != nullptr) {
+                    cout << "\nApakah Anda ingin menghapus tim? (y/n): ";
+                    char pilihan;
+                    cin >> pilihan;
+                    cin.ignore();
+
+                    if (pilihan == 'y' || pilihan == 'Y') {
+                        cout << "Masukkan nama tim yang akan dihapus: ";
+                        getline(cin, teamName);
+                        removeTeam(head, teamName);
+                    }
+                }
+                break;
+
+            case 3:
+                cout << "Masukkan nama tim: ";
+                getline(cin, teamName);
+                cout << "Masukkan posisi (1-" << (head == nullptr ? 1 : 2) << "): ";
+                int position;
+                cin >> position;
+                insertTeam(head, teamName, position);
+                break;
+
+            case 4:
+                cout << "Masukkan nama tim yang akan dikelola playernya: ";
+                getline(cin, teamName);
+                managePlayersMenu(head, teamName);
+                break;
+
+            case 5:
+                displayAllTeams(head);
+                break;
+
+            case 6:
+                cout << "Masukkan nama tim: ";
+                getline(cin, teamName);
+                displayTeam(head, teamName);
+                break;
+
+            case 7:
+                cout << "Terima kasih telah menggunakan sistem ini!\n";
+                break;
+
+            default:
+                cout << "Pilihan tidak valid! Silakan pilih 1-7.\n";
+        }
+
+        if (choice != 7) {
+            cout << "\nTekan Enter untuk melanjutkan...";
+            cin.get();
+        }
+
+    } while (choice != 7);
+
+    while (head != nullptr) {
+        TeamNode* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    return 0;
+}
