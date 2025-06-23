6bc198e4d847a5928dd3681835e3ea32c910e8c8
Muhammad Aqshal Habibi <aqshalh13@gmail.com>
2025-06-23 20:46:51 +0700

    Pembuatan struct untuk Player dan Team

---
+++ LinkedList.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+struct Player {
+    string nickname;
+    string id;
+    string server;
+    
+    Player() : nickname(""), id(""), server("") {}
+    Player(string nick, string playerId, string srv) : nickname(nick), id(playerId), server(srv) {}
+};
+
+// Struktur untuk node tim dalam linked list
+struct TeamNode {
+    string teamName;
+    Player players[5];  // Array 5 player per tim
+    int playerCount;    // Jumlah player yang sudah terdaftar
+    TeamNode* next;
+    
+    TeamNode(string name) : teamName(name), playerCount(0), next(nullptr) {}
+};
+
+
+void showMenu() {
+    cout << "\n=== SISTEM PENDAFTARAN TURNAMEN MOBILE LEGENDS ===\n";
+    cout << "1. Tambah Tim Baru\n";
+    cout << "2. Lihat & Hapus Tim\n";
+    cout << "3. Insert Tim di Posisi Tertentu\n";
+    cout << "4. Kelola Player Tim\n";
+    cout << "5. Tampilkan Semua Tim (Detail)\n";
+    cout << "6. Tampilkan Detail Tim Tertentu\n";
+    cout << "7. Keluar\n";
+    cout << "Pilih menu (1-7): ";
+}
